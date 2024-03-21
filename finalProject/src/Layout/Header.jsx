e9518febf9e58d9d1d417a5aaf495297a64d8584
Annemoon_de_Groen <annemoon2003@gmail.com>
2024-03-21 15:29:12 +0100

    Some small bug fixes

--- finalProject/src/Layout/Header.jsx
+++ finalProject/src/Layout/Header.jsx
@@ -1,11 +1,22 @@
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import '../Stylesheets/header.css'
 import { userContext } from '../App'
 import { useNavigate } from 'react-router-dom'
+import { GetUserById } from '../Services/ConnectToDB'
 
 function Header() {
-    const { user } = useContext(userContext)
+    const [user, setUser] = useState(null)
     const navigate = useNavigate()
+    useEffect(() => {
+        GetUserById(localStorage.getItem("user")).then((response) => {
+            setUser(response)
+        }
+        )
+    });
+
+    if (!user) { return (<h1 className='header'>Loading...</h1>) }
+
+
     return (
         <div className="header">
             <h1 onClick={() => { navigate("/") }} >Whisper Rose</h1>
